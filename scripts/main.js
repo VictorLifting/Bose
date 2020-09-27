905d93e700a4ea8f372ca1e804a7ed73ad2df35a
victor <contactovictore@gmail.com>
2020-09-27 11:29:40 -0500

    avance en la galeria

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,15 @@
 const carrousel = document.querySelector('.carrousel');
 const carrouselStripe = document.querySelector('.carrousel__stripe');
+let current=0;
 
 carrousel.addEventListener('click', function(){
+    current++;
+    if(current>3){
+        current=0;
+    }
 
-    carrouselStripe.style.transform ='translate(-1500px, 0px)'
+
+    carrouselStripe.style.transform ='translate(-'+(1500*current)+'px, 0px)'
+    console.log(800*current)
+
 })
