9ae1da6ed3862b6aaf49af65aeb8fc25fad49e0b
victor <contactovictore@gmail.com>
2020-09-27 00:45:32 -0500

    añado js y empiezo carrusel

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const carrousel = document.querySelector('.carrousel');
+const carrouselStripe = document.querySelector('.carrousel__stripe');
+
+carrousel.addEventListener('click', function(){
+
+    carrouselStripe.style.transform ='translate(-500px, 0px)'
+})
