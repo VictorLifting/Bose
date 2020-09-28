92b611e75d9fe57b27d8d33fcee85d70454042d6
victor <contactovictore@gmail.com>
2020-09-28 06:01:13 -0500

    detalles

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 const carrousel = document.querySelector('.carrousel');
 const carrouselStripe = document.querySelector('.carrousel__stripe');
+let btnInteraction = document.querySelectorAll('btn btn--interaction');
 let current=0;
 
 carrousel.addEventListener('click', function(){
@@ -7,8 +8,17 @@ carrousel.addEventListener('click', function(){
     if(current>3){
         current=0;
     }
-    
+
     carrouselStripe.style.transform ='translate(-'+(1500*current)+'px, 0px)'
     console.log(800*current)
 
 })
+
+for (let i = 0; i < btnInteraction.length; i++) {
+    console.log('holas');
+     
+    
+
+    
+
+}
