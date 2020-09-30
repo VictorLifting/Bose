ad7a2f64fec66c763a766cd7d8cc9b10e4560829
victor <contactovictore@gmail.com>
2020-09-30 05:44:36 -0500

    arreglos

--- scripts/main.js
+++ scripts/main.js
@@ -6,11 +6,11 @@ let current=0;
 
 carrousel.addEventListener('click', function(){
     current++;
-    if(current>3){
+    if(current>2){
         current=0;
     }
 
-    carrouselStripe.style.transform ='translate(-'+(1500*current)+'px, 0px)'
+    carrouselStripe.style.transform ='translate(-'+(1920*current)+'px, 0px)'
     console.log(800*current)
 
 })
