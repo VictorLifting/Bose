46b22029f7775f79b808c66eb0816aa437c2466b
victor <contactovictore@gmail.com>
2020-11-08 23:33:50 -0500

    empiezo el registro

---
+++ scripts/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCpN03-M6EEOmqPg1oJYmrDsQlc3KgpEcY",
+    authDomain: "bose-7ede1.firebaseapp.com",
+    databaseURL: "https://bose-7ede1.firebaseio.com",
+    projectId: "bose-7ede1",
+    storageBucket: "bose-7ede1.appspot.com",
+    messagingSenderId: "483009449933",
+    appId: "1:483009449933:web:ebec51372b0376fb3f7e84",
+    measurementId: "G-027XFDSE3N"
+  };
--- scripts/tienda.js
+++ scripts/tienda.js
@@ -1,13 +1,4 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCpN03-M6EEOmqPg1oJYmrDsQlc3KgpEcY",
-  authDomain: "bose-7ede1.firebaseapp.com",
-  databaseURL: "https://bose-7ede1.firebaseio.com",
-  projectId: "bose-7ede1",
-  storageBucket: "bose-7ede1.appspot.com",
-  messagingSenderId: "483009449933",
-  appId: "1:483009449933:web:ebec51372b0376fb3f7e84",
-  measurementId: "G-027XFDSE3N"
-};
+
 // Initialize Firebase
 
 firebase.initializeApp(firebaseConfig);
@@ -115,7 +106,10 @@ let selectedItem = null;
   console.log('Uploaded a blob or file!');
 });
 
-return;
+
+
+//para probar la subida de la imagen
+//return;
   
     const newProduct = {
       title: form.title.value,
