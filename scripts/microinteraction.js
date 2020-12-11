1c481ab6da389a5e80880ef6276138031517e4be
victor <contactovictore@gmail.com>
2020-12-11 00:57:43 -0500

    microinteraion

--- scripts/microinteraction.js
+++ scripts/microinteraction.js
@@ -6,14 +6,11 @@ window.addEventListener('load', function() {
 
     const cart = document.querySelectorAll('.sec__img');
 
-
-
-  
-      const rot = gsap.timeline({  });
-      rot.to(cart, { rotation: 20, duration: .3 });
-      rot.to(cart, { rotation: -40, duration: .6 });
+      const rot = gsap.timeline({ repeat: -1, repeatDelay: 2 });
+      rot.to(cart, { rotation: 5, duration: .3 });
+      rot.to(cart, { rotation: -10, duration: .6 });
       rot.to(cart, { rotation: 0, duration: .6 });
-      pos.delay(.06);
+ 
   
       // pos.timeScale(.4);
       // rot.timeScale(.4);
